bfd299eb923b650c17fd75e281159e8259c158e7
kolostring <yoerodriguezjunior@gmail.com>
2025-02-16 07:13:42 -0500

    feat: add images and new screen styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				inter: ['Inter', 'sans-serif'],
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+
+      screens: {
+        "3xl": "1920px",
+      },
+    },
+  },
+  plugins: [],
+};
